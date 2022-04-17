4b1aa1b4e0dd99551b4fa72e9dd0184f22430603
shahryar tavakkoli <shahryar.tbiz@gmail.com>
2022-04-17 14:06:04 +0430

    add some comment for http sender and delete example code for finch

--- lib/chat_f_coin/helper/http_sender.ex
+++ lib/chat_f_coin/helper/http_sender.ex
@@ -2,6 +2,8 @@ defmodule ChatFCoin.Helper.HttpSender do
   @url "https://graph.facebook.com/v2.6/me/messages"
   @request_name MyHttpClient
 
+  # If there is another chatbot like telegram, I prefer to change this HTTP sender module as helper not for specific social network
+  # Not it is like a hardcode sender and it is not my type in coding
   @spec send_message(any, binary | URI.t()) :: {:error, Exception.t} | {:ok, Finch.Response.t()}
   def send_message(body, url \\ @url) do
     access_token = ChatFCoin.get_config(:facebook_chat_accsess_token)
@@ -35,33 +37,3 @@ defmodule ChatFCoin.Helper.HttpSender do
   # I should pass nil or error as atom, but the message can be sent with Dear client and make him/her smile With our respect
   defp handle_user_info(_), do: %{"first_name" => "Dear client", "last_name" => "", "profile_pic" => "", "id" => ""}
 end
-
-  # def send_card(psid, entry) do
-  #   url = "#{@facebook_url}?access_token=#{Application.get_env(:rent_bot_web, RentBotWeb.BotController)[:facebook_messenger_access_token]}"
-  #   body = %{
-  #     recipient: %{id: psid},
-  #     message: %{
-  #       attachment: %{
-  #         type: "template",
-  #         payload: %{
-  #           template_type: "generic",
-  #           elements: [
-  #             %{
-  #               title: entry.title,
-  #               image_url: entry.image,
-  #               subtitle: entry.price,
-  #               default_action: %{
-  #                 type: "web_url",
-  #                 url: entry.url,
-  #                 messenger_extensions: false,
-  #                 webview_height_ratio: "full"
-  #               }
-  #             }
-  #           ]
-  #         }
-  #       }
-  #     }
-  #   }
-  #   headers = [{"Content-type", "application/json"}]
-  #   HTTPoison.post(url, Poison.encode!(body), headers)
-  # end
