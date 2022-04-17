4e4314c91ff088a8af310ada5396bc096e92157f
shahryar tavakkoli <shahryar.tbiz@gmail.com>
2022-04-17 16:06:54 +0430

    add push call and get, get all for user msg state

--- lib/chat_f_coin/state/user_msg_genserver.ex
+++ lib/chat_f_coin/state/user_msg_genserver.ex
@@ -9,7 +9,7 @@ defmodule ChatFCoin.UserMsgDynamicGenserver do
   @type user_info() :: map()
   @type user_answers() :: [integer()]
   @type last_try() :: NaiveDateTime.t()
-  @type parent_pid() :: pid()
+  @type parent_pid() :: pid() | nil
   @type social_network() :: :facebook, :telegram
   @type user_msg() :: %UserMsgDynamicGenserver{
     user_id: user_id(),
@@ -20,6 +20,7 @@ defmodule ChatFCoin.UserMsgDynamicGenserver do
   }
   @type t :: user_msg()
 
+  # Sender
   def start_link(args) do
     {id, type, parent_pid} = {Map.get(args, :id), Map.get(args, :type), Map.get(args, :parent_pid)}
     GenServer.start_link(__MODULE__, default(id, type, parent_pid), name: via(id, type))
@@ -38,6 +39,29 @@ defmodule ChatFCoin.UserMsgDynamicGenserver do
     %UserMsgDynamicGenserver{user_id: user_id, parent_pid: parent_pid, social_network: type, last_try: NaiveDateTime.utc_now()}
   end
 
+  def push_call(%UserMsgDynamicGenserver{} = element) do
+    case UserMsgDynamicSupervisor.start_job(%{id: element.user_id, type: element.social_network, parent_pid: element.parent_pid}) do
+      {:ok, status, pid} ->
+        if Mix.env() == :test, do: Logger.warn("#{element.user_info["first_name"]} is being pushed")
+        GenServer.call(pid, {:push, status, element})
+      {:error, result} ->  {:error, :push, result}
+    end
+  end
+
+  def get(network: network_name) do
+    case UserMsgDynamicSupervisor.get_user_msg_pid(network_name) do
+      {:ok, :get_user_msg_pid, pid} -> GenServer.call(pid, {:pop, :network})
+      {:error, :get_user_msg_pid} -> {:error, :get, :not_found}
+    end
+  end
+
+  def get_all(network: network_name) do
+    UserMsgDynamicSupervisor.running_imports(network_name) |> Enum.map(&get(network: &1.id))
+  end
+
+  def get_all() do
+    UserMsgDynamicSupervisor.running_imports() |> Enum.map(&get(network: &1.id))
+  end
 
   # Callbacks
   @impl true
@@ -50,6 +74,16 @@ defmodule ChatFCoin.UserMsgDynamicGenserver do
   @impl true
   def handle_continue({:user_evaluation}, %UserMsgDynamicGenserver{} = state) do
     {:noreply, state}
+  end
+
+  @impl true
+  def handle_call({:push, _status, %UserMsgDynamicGenserver{} = element}, _from, %UserMsgDynamicGenserver{} = _state) do
+    {:reply, element, element}
+  end
+
+  @impl true
+  def handle_call({:pop, :network}, _from, %UserMsgDynamicGenserver{} = state) do
+    {:reply, state, state}
   end
 
   defp via(id, value) do
